feecefc9b23994ce655b8c347dc1843a5871a01d
Rich2 <rr1@tutanota.com>
2024-10-20 22:49:06 +0100

    Scala back to 3.5.1.

--- Main.scala
+++ Main.scala
@@ -1,10 +1,11 @@
+/* Copyright 2024 Richard Oliver. Licensed under Apache Licence version 2.0. */
 import java.net.*, java.io.*
 
 object Main
 {
   def main(args: Array[String]): Unit =
   { println("RichSand starting.")
-    val sock: Socket = new Socket("richstrat.com", 8080)
+    val sock: Socket = new Socket("richstrat.com", 8082)
     val out: PrintWriter = new PrintWriter(sock.getOutputStream(), true)
     val in: BufferedReader = new BufferedReader(new InputStreamReader(sock.getInputStream()))
 
--- RichSand.sbt
+++ RichSand.sbt
@@ -1,6 +1,7 @@
+/* Copyright 2024 Richard Oliver. Licensed under Apache Licence version 2.0. */
 name:= "RichSand"
 version := "0.0.1snap"
-scalaVersion := "3.5.2"
+scalaVersion := "3.5.1"
 resolvers += "RichStrat" at "https://richstrat.com/repository"
 libraryDependencies += "com.richstrat" % "rutil" % "0.3.4" withSources() withJavadoc()
 libraryDependencies += "com.richstrat" % "geom" % "0.3.4" withSources() withJavadoc()
