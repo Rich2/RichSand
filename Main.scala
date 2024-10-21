0f057ed60ccad22d74f14348c8addffeaceb7395
Rich2 <rr1@tutanota.com>
2024-10-21 01:20:37 +0100

    HTTP response.

--- Main.scala
+++ Main.scala
@@ -1,13 +1,16 @@
 /* Copyright 2024 Richard Oliver. Licensed under Apache Licence version 2.0. */
-import java.net.*, java.io.*
+import java.net.*, java.io.*, ostrat.*
 
 object Main
 {
   def main(args: Array[String]): Unit =
   { println("RichSand starting.")
-    val sock: Socket = new Socket("richstrat.com", 8082)
+    val sock: Socket = new Socket("richstrat.com", 80)
     val out: PrintWriter = new PrintWriter(sock.getOutputStream(), true)
     val in: BufferedReader = new BufferedReader(new InputStreamReader(sock.getInputStream()))
+    out.write("GET / HTTP/1.1\r\nHost: richstrat.com:80\r\n\r\n")
+    out.flush
+    repeat(8){ println(in.readLine) }
 
     in.close
     out.close
