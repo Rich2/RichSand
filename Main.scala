d0e4811a53e7ceb61af50abda31002bd9e45d346
Rich2 <rr1@tutanota.com>
2024-10-30 13:49:32 +0000

    Now using JavaFx 21.

--- Main.scala
+++ Main.scala
@@ -1,19 +1,25 @@
 /* Copyright 2024 Richard Oliver. Licensed under Apache Licence version 2.0. */
-import java.net.*, java.io.*, ostrat.*
+import java.net.*, java.io.*, ostrat.*, javafx.*, stage.*, scene.*, layout.StackPane, control.Button, event.*
 
 object Main
 {
   def main(args: Array[String]): Unit =
-  { println("RichSand starting.")
-    val sock: Socket = new Socket("richstrat.com", 80)
-    val out: PrintWriter = new PrintWriter(sock.getOutputStream(), true)
-    val in: BufferedReader = new BufferedReader(new InputStreamReader(sock.getInputStream()))
-    out.write("GET / HTTP/1.1\r\nHost: richstrat.com:80\r\n\r\n")
-    out.flush
-    repeat(8){ println(in.readLine) }
-
-    in.close
-    out.close
-    sock.close
+  { javafx.application.Application.launch(classOf[AppStart], args*)
   }
-} 
+}
+class AppStart extends application.Application
+{
+  override def start(stage0: Stage): Unit =
+  { println("RichSand starting.")
+    stage0.setTitle("Rich Sand")
+    val root = new StackPane
+    val butt = new Button()
+    butt.setText("Say Hello Sandbox")
+    butt.setOnAction(new EventHandler[ActionEvent] {
+      @Override def handle(event: ActionEvent) = System.out.println("Hello Sandbox!")
+    })
+    root.getChildren().add(butt)
+    stage0.setScene(new Scene(root, 400, 280))
+    stage0.show
+  }
+}
--- RichSand.sbt
+++ RichSand.sbt
@@ -1,9 +1,10 @@
 /* Copyright 2024 Richard Oliver. Licensed under Apache Licence version 2.0. */
 name:= "RichSand"
 version := "0.0.1snap"
-scalaVersion := "3.5.1"
+scalaVersion := "3.5.2"
 resolvers += "RichStrat" at "https://richstrat.com/repository"
 libraryDependencies += "com.richstrat" % "rutil" % "0.3.4" withSources() withJavadoc()
 libraryDependencies += "com.richstrat" % "geom" % "0.3.4" withSources() withJavadoc()
 libraryDependencies += "com.richstrat" % "tiling" % "0.3.4" withSources() withJavadoc()
 libraryDependencies += "com.richstrat" % "egrid" % "0.3.4" withSources() withJavadoc()
+libraryDependencies += "org.openjfx" % "javafx-controls" % "21.0.5" withSources() withJavadoc()
