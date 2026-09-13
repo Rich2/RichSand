a60b3c8ef1ab5430ac6eef49b38b79a1dc689e98
Rich2 <rr1@tutanota.com>
2026-09-13 15:17:44 +0100

    Updated gitignore.

--- Core/src/Main.scala
+++ Core/src/Main.scala
@@ -1,10 +1,17 @@
 /* Copyright 2026 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package pcore
-import ostrat.*
+import ostrat.*, webjvm.*
 
 object MainApp
 {
   def main(args: Array[String]): Unit =
   { deb("At moment just to test ScalaDoc.")
+
+    deb("Welcome to PostApp!")
+    val eStr: IOExcEither[String] = resourceStr("Postgres.rson")
+    val eName = eStr.flatMap(_.findStrSetting("username"))
+    val ePass = eStr.flatMap(_.findStrSetting("pWord"))
+    debvar(eName)
+    debvar(ePass)
   }
 }
